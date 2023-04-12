a2f2c195a7587e88b39c4b468574fc1abf2e076e
sweedalgharshi <sweedalgracius@gmail.com>
2023-04-12 12:21:16 +0530

    added rightbar components

--- react-social/src/components/rightbar/Rightbar.js
+++ react-social/src/components/rightbar/Rightbar.js
@@ -2,7 +2,57 @@ import React from "react";
 import "./rightbar.css";
 
 function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbar-wrapper">
+        <div className="birthday-container">
+          <img src="/assets/gift.png" className="birthday-img" alt="" />
+          <span className="birthday-text">
+            <b>Pola Foster</b> and <b>3 other friends</b> have birthday today.
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="" className="rightbar-ad" />
+        <h4 className="rightbar-title">Online Friends</h4>
+        <ul className="rightbar-friend-list">
+          <li className="rightbar-friend">
+            <div className="rightbar-profile-img-container">
+              <img src="/assets/person/3.jpeg" alt="" className="rightbar-profile-img" />
+              <span className="rightbar-online"></span>
+            </div>
+            <span className="rightbar-username">Kelly Wilson</span>
+          </li>
+          <li className="rightbar-friend">
+            <div className="rightbar-profile-img-container">
+              <img src="/assets/person/3.jpeg" alt="" className="rightbar-profile-img" />
+              <span className="rightbar-online"></span>
+            </div>
+            <span className="rightbar-username">Kelly Wilson</span>
+          </li>
+          <li className="rightbar-friend">
+            <div className="rightbar-profile-img-container">
+              <img src="/assets/person/3.jpeg" alt="" className="rightbar-profile-img" />
+              <span className="rightbar-online"></span>
+            </div>
+            <span className="rightbar-username">Kelly Wilson</span>
+          </li>
+          <li className="rightbar-friend">
+            <div className="rightbar-profile-img-container">
+              <img src="/assets/person/3.jpeg" alt="" className="rightbar-profile-img" />
+              <span className="rightbar-online"></span>
+            </div>
+            <span className="rightbar-username">Kelly Wilson</span>
+          </li>
+          <li className="rightbar-friend">
+            <div className="rightbar-profile-img-container">
+              <img src="/assets/person/3.jpeg" alt="" className="rightbar-profile-img" />
+              <span className="rightbar-online"></span>
+            </div>
+            <span className="rightbar-username">Kelly Wilson</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Rightbar;
