e19c31f6f17f90a20e1e2858d7c64a43193618d2
sweedalgharshi <sweedalgracius@gmail.com>
2023-04-12 12:06:01 +0530

    added post components

--- react-social/src/components/feed/Feed.js
+++ react-social/src/components/feed/Feed.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./feed.css";
 
 import Share from "../share/Share";
+import Post from "../post/Post";
 
 function Feed() {
   return (
     <div className="feed">
       <div className="feed-wrapper">
         <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
